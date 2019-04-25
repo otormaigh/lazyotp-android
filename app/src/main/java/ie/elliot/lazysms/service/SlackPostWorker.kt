f74e93ec122c449754c9ef0df6ba7dc99decc1de
Elliot Tormey <elliot.tormey@gmail.com>
2019-04-25 19:55:44 +0100

    reformat slack message to make it more descriptive

--- app/src/main/java/ie/elliot/lazysms/service/SlackPostWorker.kt
+++ app/src/main/java/ie/elliot/lazysms/service/SlackPostWorker.kt
@@ -2,21 +2,30 @@ package ie.elliot.lazysms.service
 
 import android.content.Context
 import androidx.work.CoroutineWorker
+import androidx.work.Data
 import androidx.work.WorkerParameters
 import ie.elliot.lazysms.api.Api
-import timber.log.Timber
+import ie.elliot.lazysms.toolbox.extension.phoneNumber
 
 class SlackPostWorker(context: Context, workerParams: WorkerParameters) : CoroutineWorker(context, workerParams) {
   override suspend fun doWork(): Result {
+    val phoneNumber = applicationContext.phoneNumber
+    val sender = inputData.getString(ARG_SENDER)
     val smsCode = inputData.getString(ARG_SMS_CODE)
+    val message = """{ "text":"*phone*: $phoneNumber\n*sender*: $sender\n*code*: $smsCode" }""".trimIndent()
 
-    val payload = """{"text":"$smsCode"}""".trimIndent()
-    Timber.e(payload)
-    Api.instance.postSmsCode(payload).await()
+    Api.instance.postSmsCode(message).await()
+
     return Result.success()
   }
 
   companion object {
-    const val ARG_SMS_CODE = "arg.sms_code"
+    private const val ARG_SMS_CODE = "arg.sms_code"
+    private const val ARG_SENDER = "arg.sender"
+
+    fun data(sender: String, smsCode: String) = Data.Builder().apply {
+      putString(ARG_SENDER, sender)
+      putString(ARG_SMS_CODE, smsCode)
+    }.build()
   }
 }
