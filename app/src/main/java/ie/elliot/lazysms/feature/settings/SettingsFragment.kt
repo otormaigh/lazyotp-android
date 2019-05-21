4c46f64c822c83f2ddb26443793c03a12deaeab8
Elliot Tormey <elliot.tormey@gmail.com>
2019-05-21 20:24:38 +0100

    add send test message button to settings menu

--- app/src/main/java/ie/elliot/lazysms/feature/settings/SettingsFragment.kt
+++ app/src/main/java/ie/elliot/lazysms/feature/settings/SettingsFragment.kt
@@ -1,11 +1,24 @@
 package ie.elliot.lazysms.feature.settings
 
 import android.os.Bundle
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import ie.elliot.lazysms.R
+import ie.elliot.lazysms.service.SlackPostWorker
+import ie.elliot.lazysms.toolbox.WorkScheduler
 
 class SettingsFragment : PreferenceFragmentCompat() {
   override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
     addPreferencesFromResource(R.xml.prefs_settings)
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    findPreference<Preference>("btnSendTest")?.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+      WorkScheduler.oneTimeRequest<SlackPostWorker>(requireContext(), SlackPostWorker.data("Test", "012345"))
+
+      true
+    }
   }
 }
