f7729aa8610903794e724cf0aa587b1251cf6e3c
Elliot Tormey <elliot.tormey@gmail.com>
2021-03-06 17:19:21 +0000

    update LazySmsApi.postSmsCode to replace url encoded payload with RequestBody/Body

--- app/src/main/java/ie/otormaigh/lazyotp/api/LazySmsApi.kt
+++ app/src/main/java/ie/otormaigh/lazyotp/api/LazySmsApi.kt
@@ -1,17 +1,15 @@
 package ie.otormaigh.lazyotp.api
 
-import ie.otormaigh.lazyotp.BuildConfig
+import okhttp3.RequestBody
 import retrofit2.Response
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
+import retrofit2.http.Body
 import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface LazySmsApi {
   @POST("{token}")
-  @FormUrlEncoded
   suspend fun postSmsCode(
-    @Field("payload") payload: String,
-    @Path("token", encoded = true) token: String = BuildConfig.SLACK_TOKEN
+    @Body payload: RequestBody,
+    @Path("token", encoded = true) token: String
   ): Response<Unit>
 }
