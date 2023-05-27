06567bf2025ecd8e82a11bdffcc9392da13cb83a
Elliot Tormey <elliot.tormey@gmail.com>
2023-05-27 13:05:11 +0100

    clean up SettingsActivity/Fragment

--- app/src/main/java/ie/otormaigh/lazyotp/feature/settings/SettingsActivity.kt
+++ app/src/main/java/ie/otormaigh/lazyotp/feature/settings/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package ie.otormaigh.lazyotp.feature.settings
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import ie.otormaigh.lazyotp.R
 
@@ -9,5 +10,17 @@ class SettingsActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_settings)
     supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, SettingsFragment()).commit()
+
+    supportActionBar?.apply {
+      setDisplayHomeAsUpEnabled(true)
+      title = getString(R.string.title_settings)
+    }
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    when (item.itemId) {
+      android.R.id.home -> finish()
+    }
+    return super.onOptionsItemSelected(item)
   }
 }
