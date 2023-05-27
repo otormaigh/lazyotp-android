48290b65924fad121fb3e7d116f175ac2f5471e8
Elliot Tormey <elliot.tormey@gmail.com>
2023-05-27 13:05:11 +0100

    clean up SettingsActivity/Fragment

--- app/src/main/java/ie/otormaigh/lazyotp/feature/settings/SettingsFragment.kt
+++ app/src/main/java/ie/otormaigh/lazyotp/feature/settings/SettingsFragment.kt
@@ -1,6 +1,7 @@
 package ie.otormaigh.lazyotp.feature.settings
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import ie.otormaigh.lazyotp.BuildConfig
@@ -10,6 +11,7 @@ import ie.otormaigh.lazyotp.service.SlackPostWorker
 import ie.otormaigh.lazyotp.toolbox.WorkScheduler
 import ie.otormaigh.lazyotp.toolbox.batteryWarningEnabled
 import ie.otormaigh.lazyotp.toolbox.settingsPrefs
+import ie.otormaigh.lazyotp.toolbox.slackToken
 
 class SettingsFragment : PreferenceFragmentCompat() {
   override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
@@ -19,31 +21,40 @@ class SettingsFragment : PreferenceFragmentCompat() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
+    setBatteryThresholdItemVisibility()
+
     findPreference<Preference>("btnSendTest")?.onPreferenceClickListener = Preference.OnPreferenceClickListener {
-      WorkScheduler.oneTimeRequest<SlackPostWorker>(
-        requireContext(),
-        SlackPostWorker.smsCodeData(
-          "Test",
-          "012345",
-          System.currentTimeMillis()
-        )
-      )
-
-      true
-    }
-
-    findPreference<Preference>("key_battery_warning")?.onPreferenceClickListener = Preference.OnPreferenceClickListener {
-      if (context?.settingsPrefs?.batteryWarningEnabled == true) {
-        findPreference<Preference>("key_battery_threshold")?.isVisible = true
-        BatteryLevelService.start(requireContext())
+      if (context?.settingsPrefs?.slackToken.isNullOrEmpty()) {
+        Toast.makeText(context, getString(R.string.slack_token_empty), Toast.LENGTH_LONG).show()
       } else {
-        findPreference<Preference>("key_battery_threshold")?.isVisible = false
-        BatteryLevelService.stop(requireContext())
+        WorkScheduler.oneTimeRequest<SlackPostWorker>(
+          requireContext(),
+          SlackPostWorker.smsCodeData(
+            "Test",
+            "012345",
+            System.currentTimeMillis()
+          )
+        )
       }
 
       true
     }
 
+    findPreference<Preference>("key_battery_warning")?.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+      setBatteryThresholdItemVisibility()
+      true
+    }
+
     findPreference<Preference>("tvAppVersion")?.summary = BuildConfig.VERSION_NAME
+  }
+
+  private fun setBatteryThresholdItemVisibility() {
+    if (context?.settingsPrefs?.batteryWarningEnabled == true) {
+      findPreference<Preference>("key_battery_threshold")?.isVisible = true
+      BatteryLevelService.start(requireContext())
+    } else {
+      findPreference<Preference>("key_battery_threshold")?.isVisible = false
+      BatteryLevelService.stop(requireContext())
+    }
   }
 }
