fa9fa2232622e00943c0958b4fc43c989360b4e6
Elliot Tormey <elliot.tormey@gmail.com>
2024-03-23 15:01:32 +0000

    replace kapt with KSP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,8 @@
-buildscript {
-  repositories {
-    google()
-    mavenCentral()
-  }
-  dependencies {
-    classpath(libs.android.build)
-    classpath(libs.kotlin.plugin)
-    classpath(libs.google.hilt.plugin)
-  }
-}
-
-allprojects {
-  repositories {
-    google()
-    mavenCentral()
-  }
+plugins {
+  alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.kotlin.android) apply false
+  alias(libs.plugins.google.hilt) apply false
+  alias(libs.plugins.google.ksp) apply false
 }
 
 tasks.register("clean", Delete::class) {
